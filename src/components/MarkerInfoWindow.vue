03c2d0b0ddee38384092040f84c529454cde216e
Filippo Nardi <filippo.nardi92@gmail.com>
2018-10-21 16:54:11 +0200

    Hours for present day

--- src/components/MarkerInfoWindow.vue
+++ src/components/MarkerInfoWindow.vue
@@ -4,6 +4,8 @@
         <h2>{{store.storeName}}</h2>
         <span><b>Indirizzo</b>: {{ address }}</span>
         <span><b>Telefono</b>: {{ telephone }}</span>
+        <span><b>Orari di Oggi</b>: {{printDay}}</span>
+        <span><b>apertura</b>: {{ openingHour}}, <b>chiusura</b>: {{ closingHour }} </span>
       </div>
     </div>
 </template>
@@ -16,11 +18,25 @@ export default {
     };
   },
   computed: {
+    getDay() {
+      let day = new Date();
+      return day.getDay();
+    },
+    printDay() {
+      let day = ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'];
+      return day[this.getDay];
+    },
     address() {
       return this.store.address.line1;
     },
     telephone() {
       return this.store.contacts[0].value;
+    },
+    openingHour() {
+      return this.store.openingTimes[this.getDay].slots[0].openTime;
+    },
+    closingHour() {
+      return this.store.openingTimes[this.getDay].slots[0].closeTime;
     }
   }
 };
